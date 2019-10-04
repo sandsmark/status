6310ffabdfafd4284675d29cf3fc51f9d3be5f67
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2019-10-04 16:59:56 +0200

    add support for showing notifications

--- main.cc
+++ main.cc
@@ -508,7 +508,144 @@ static void print_volume(PulseClient &client)
     }
 }
 
+struct Notification {
+    std::string app, message;
+    int timeout = 0;
+};
+
+static void print_notification(Notification *notification)
+{
+    std::string message;
+    if (!notification->app.empty()) {
+        message += notification->app + ": ";
+    }
+    message += notification->message;
+    if (message.size() > 50) {
+        message.resize(50);
+        message += "...";
+    }
+
+    fputs(message.c_str(), stdout);
+    print_green();
+}
+
+static std::vector<Notification> g_notifications;
+
+static int method_notify(sd_bus_message *m, void * /*userdata*/, sd_bus_error *error)
+{
+  //printf("Got called with userdata=%p\n", userdata);
+
+  const char *app_name = nullptr;
+  uint32_t replaces_id = 0u;
+  const char *app_icon = nullptr;
+  const char *summary = nullptr;
+  const char *body = nullptr;
+
+  int timeout = 0;
+
+  int ret = sd_bus_message_read(m, "susss", &app_name, &replaces_id, &app_icon, &summary, &body);
+  if (ret < 0) {
+      fprintf(stderr, "Failed to parse parameters: %s\n", strerror(-ret));
+      return ret;
+  }
+
+  // Can't be bothered to parse these, which aren't used
+  ret = sd_bus_message_skip(m, "asa{ss}");
+  if (ret < 0) {
+      fprintf(stderr, "Failed to skip parameters: %s\n", strerror(-ret));
+  }
+
+  ret = sd_bus_message_read(m, "i", &timeout);
+  if (ret < 0) {
+      fprintf(stderr, "Failed to parse timeout: %s\n", strerror(-ret));
+      return ret;
+  }
+
+  /* Return an error on division by zero */
+  if (false) {
+      sd_bus_error_set_const(error, "net.poettering.DivisionByZero", "Sorry, can't allow division by zero.");
+      return -EINVAL;
+  }
+  if (!g_notifications.empty()) {
+      if (g_notifications.size() > 5) { // don't have too many..
+          g_notifications.resize(5);
+      }
+
+      // Clean out old ones
+      for (Notification &notification : g_notifications) {
+          notification.timeout = 1;
+      }
+  }
+
+  Notification notification;
+  notification.app = app_name;
+  notification.message = summary;
+  notification.timeout = std::max(timeout, 1000) / 1000;
+  g_notifications.push_back(std::move(notification));
+
+  static int id = 0;
+  fprintf(stderr, "%s %u %s %s %s %d\n", app_name, replaces_id, app_icon, summary, body, timeout);
+
+  return sd_bus_reply_method_return(m, "u", id++);
+}
+
+static const sd_bus_vtable notifications_vtable[] = {
+    SD_BUS_VTABLE_START(0),
+
+    SD_BUS_METHOD("Notify",
+                "s"      // app_name
+                "u"      // replaces_id
+                "s"      // app_icon
+                "s"      // summary
+                "s"      // body
+                "as"     // actions
+                "a{ss}"  // hints
+                "i",     // timeout
+            "u",     // out
+            method_notify,
+        SD_BUS_VTABLE_UNPRIVILEGED),
+
+    SD_BUS_VTABLE_END
+};
+
 static bool g_running = true;
+
+bool register_notification_service(sd_bus *bus, sd_bus_slot **slot)
+{
+    if (!bus) {
+        fprintf(stderr, "No bus to register to\n");
+        return false;
+    }
+
+    fprintf(stderr, "Registered notifications service %p\n", *slot);
+    int ret = sd_bus_add_object_vtable(bus,
+            slot,
+            "/org/freedesktop/Notifications",  /* object path */
+            "org.freedesktop.Notifications",   /* interface name */
+            notifications_vtable,
+            NULL);
+
+    fprintf(stderr, "registered dbus vtable: %d", ret);
+    if (ret < 0) {
+        fprintf(stderr, "Failed to register dbus vtable: %s", strerror(-ret));
+        return false;
+    }
+    ret = sd_bus_request_name(bus, "org.freedesktop.Notifications", 0);
+    if (ret < 0) {
+        fprintf(stderr, "Failed to request dbus name: %s", strerror(-ret));
+        return false;
+    }
+    return true;
+}
+
+void process_bus(sd_bus *bus)
+{
+    int ret = sd_bus_process(bus, NULL);
+    if (ret < 0) {
+        fprintf(stderr, "Failed to process bus: %s\n", strerror(-ret));
+        return;
+    }
+}
 
 int main() {
     int udevSocketFd = -1;
@@ -528,6 +665,24 @@ int main() {
     } else {
         fprintf(stderr, "Failed to create udev monitor\n");
     }
+
+    sd_bus_slot *slot = nullptr;
+    sd_bus *bus = nullptr;
+    int dbus_fd = -1;
+    //int ret = 0;
+    //  sd_bus_default(&bus);
+
+    //int ret = sd_bus_open_user(&bus);
+    int ret = sd_bus_default_user(&bus);
+    if (ret < 0) {
+        fprintf(stderr, "Failed to connect to system bus: %s\n", strerror(-ret));
+        bus = nullptr;
+    } else if (register_notification_service(bus, &slot)) {
+        dbus_fd = sd_bus_get_fd(bus);
+    } else {
+        fprintf(stderr, "Not using notifications\n");
+    }
+
 
     PulseClient client("status");
 
@@ -569,6 +724,10 @@ int main() {
         print_volume(client);
         print_sep();
         print_time();
+        if (!g_notifications.empty()) {
+            print_sep();
+            print_notification(&g_notifications.front());
+        }
         printf("\" } ],\n");
         fflush(stdout);
 
@@ -582,11 +741,15 @@ int main() {
             FD_ZERO(&fdset);
             FD_SET(udevSocketFd, &fdset);
 
+            if (dbus_fd >= 0) {
+                FD_SET(dbus_fd, &fdset);
+            }
+
             timeval timeout;
             timeout.tv_sec = 0;
             timeout.tv_usec = 1000000; // 1s
 
-            if (select(udevSocketFd + 1, &fdset, 0, 0, &timeout) == -1) {
+            if (select(std::max(udevSocketFd, dbus_fd) + 1, &fdset, 0, 0, &timeout) == -1) {
                 if (errno != EINTR) {
                     perror("select failed");
                     g_udevAvailable = false;
@@ -604,6 +767,11 @@ int main() {
                     g_udevAvailable = false;
                 }
             }
+
+            if (errno == 0 && FD_ISSET(dbus_fd, &fdset)) {
+                fprintf(stderr, "processing dbus\n");
+                process_bus(bus);
+            }
         } else { // fallback if udev is unavailable
             // sleep until the next second
             struct timespec ts;
@@ -617,6 +785,12 @@ int main() {
                 return 1;
             }
         }
+
+        if (!g_notifications.empty()) {
+            if (--g_notifications.front().timeout <= 0) {
+                g_notifications.erase(g_notifications.begin());
+            }
+        }
     }
 
     if (udevMonitor) {
@@ -624,5 +798,12 @@ int main() {
     }
     if (udevConnection) {
         udev_unref(udevConnection);
+    }
+
+    if (slot) {
+        sd_bus_slot_unref(slot);
+    }
+    if (bus) {
+        sd_bus_unref(bus);
     }
 }

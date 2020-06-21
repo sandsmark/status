84731a8eb97902b40b85217d17e4c8ae54f12eb1
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2020-06-21 17:42:28 +0200

    fix build

--- main.cc
+++ main.cc
@@ -472,7 +472,7 @@ int main()
         }
 #endif
 
-        if (udevConnection.battery.valid) {
+        if (udevConnection.power.valid) {
             print_battery(&udevConnection);
             print_sep();
         }
